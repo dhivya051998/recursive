da5c774e4bc3f472a74ecd694cce7c4d7f81f15a
sathya.nn <dhivya18598@gmail.com>
2024-11-05 17:17:33 +0530

    changes update

--- recursive.js
+++ recursive.js
@@ -1,37 +1,37 @@
-const JsonA = {
-    "id": 1,
-    "name": "Alice",
-    "info": {
-        "age": 30,
-        "languages": ["English", "Spanish", "French"],
-        "address": {
-            "city": "New York",
-            "zip": "10001"
-        }
-    },
-};
+// const JsonA = {
+//     "id": 1,
+//     "name": "Alice",
+//     "info": {
+//         "age": 30,
+//         "languages": ["English", "Spanish", "French"],
+//         "address": {
+//             "city": "New York",
+//             "zip": "10001"
+//         }
+//     },
+// };
 
-const JsonB = {
-    "id": 2,
-    "name": "Alice",
-    "info": {
-        "age": 30,
-        "languages": ["French"],
-        "address": {
-            "city": "Los Angeles",
-            "state": "CA"
-        }
-    },
-    "hobbies": ["reading", "traveling"]
-};
+// const JsonB = {
+//     "id": 2,
+//     "name": "Alice",
+//     "info": {
+//         "age": 30,
+//         "languages": ["French"],
+//         "address": {
+//             "city": "Los Angeles",
+//             "state": "CA"
+//         }
+//     },
+//     "hobbies": ["reading", "traveling"]
+// };
 function mergedJSON(JsonA, JsonB) {
     Object.keys(JsonB).forEach(function (key) {
         if (key in JsonA) {
             if (Array.isArray(JsonA[key]) || Array.isArray(JsonB[key])) {
                 newArray = []
                 arrayMerge = JsonA[key].concat(JsonB[key])
-                arrayMerge.forEach((e)=>{
-                    if(!newArray.includes(e)){
+                arrayMerge.forEach((e) => {
+                    if (!newArray.includes(e)) {
                         newArray.push(e)
                     }
                 });
@@ -41,13 +41,20 @@ function mergedJSON(JsonA, JsonB) {
                 mergedJSON(JsonA[key], JsonB[key]);
             }
             else if (JsonA[key] !== JsonB[key]) {
-                JsonA[key] = [JsonA[key],JsonB[key]];
+                JsonA[key] = [JsonA[key], JsonB[key]];
             }
         } else {
             JsonA[key] = JsonB[key];
         }
     });
 };
-mergedJSON(JsonA, JsonB);
-console.log(JsonA);
-document.querySelector(".details").textContent = JSON.stringify(JsonA);
+function getJson(){
+        let objOne = document.querySelector(".jsonOne").value;
+        let objTwo = document.querySelector(".jsonTwo").value;
+    if (typeof objOne !== 'object' || typeof objTwo !== 'object') {
+        JsonA = JSON.parse(objOne);
+        JsonB = JSON.parse(objTwo);
+        mergedJSON(JsonA, JsonB);
+        document.querySelector(".preview").innerText = JSON.stringify(JsonA);
+    }
+}
